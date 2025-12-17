355674a7bf1f2270ca5920525a52bc4581999377
carreiranicolas <nicolascarreira2005@gmail.com>
2025-12-16 21:32:37 -0300

    Fiz as rotas de inicio e de home, além de suas views e também um template inicial

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def inicio(request):
+    return render(request, 'tarefas/inicio.html')
+
+def home(request):
+    return render(request, 'tarefas/home.html')
